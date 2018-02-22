c58556b3ea0cc7efac2b0853469368046bc45092
Shakir Pathman <pathmanshakir@gmail.com>
2018-02-22 15:52:58 +0100

    Activity Registration klaar

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/RegisterActivity.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.smartpullup.smartpullup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,43 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText voornaam =(EditText)findViewById(R.id.VoorNaam);
+        final EditText achternaam =(EditText)findViewById(R.id.AchterNaam);
+        final EditText email =(EditText)findViewById(R.id.Email);
+        final EditText password =(EditText)findViewById(R.id.Password);
+
+
+
+        voornaam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        achternaam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
+
+
+
+
 }
