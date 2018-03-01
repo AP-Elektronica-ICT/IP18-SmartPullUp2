f5eaad9098c6084dae1270c54adafddb0330f5ae
Mohammed La <mohammed.laghzaoui@outlook.com>
2018-03-01 19:33:00 +0100

    Implementation bluetooth connection and json from arduino

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/MainActivity.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/MainActivity.java
@@ -1,10 +1,13 @@
 package com.smartpullup.smartpullup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     public final ExerciseFragment ExerciseFragment = new ExerciseFragment();
 
@@ -16,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.Content, ExerciseFragment, "exerciseFragment");
         ft.commit();
+    }
 
-
+    public void ConnectToBar(View view){
+        Intent intentConnect = new Intent(MainActivity.this, ConnectBarActivity.class);
+        startActivity(intentConnect);
     }
 }
