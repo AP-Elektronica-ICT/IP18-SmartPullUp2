ac8069b86bdeafb2a2e3db8252ae6fe08ccc9220
Jorren schepers <s094476@ap.be>
2018-03-10 14:47:15 +0100

    added temporary logout button

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/MainActivity.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/MainActivity.java
@@ -10,9 +10,12 @@ import android.view.MenuItem;
 import android.support.design.widget.BottomNavigationView;
 import android.view.View;
 
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
 
     public final ExerciseFragment ExerciseFragment = new ExerciseFragment();
 
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
        BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.navigation);
@@ -33,23 +38,32 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.action_item1:
                                 selectedFragment = ExerciseFragment.newInstance();
+                                startTransaction(selectedFragment);
                                 break;
                             case R.id.action_item2:
                                 selectedFragment = ProfileFragment.newInstance();
+                                startTransaction(selectedFragment);
+                                break;
+                            case R.id.logout:
+                                mAuth.signOut();
+                                LoginManager.getInstance().logOut();
+                                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(i);
                                 break;
                         }
 
-                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.frame_layout, selectedFragment);
-                        transaction.commit();
                         return true;
                     }
                 });
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, ExerciseFragment.newInstance());
-        transaction.commit();
+        startTransaction(ExerciseFragment.newInstance());
 
+    }
+
+    private void startTransaction(Fragment selectedFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, selectedFragment);
+        transaction.commit();
     }
 
     public void ConnectToBar(View view) {
