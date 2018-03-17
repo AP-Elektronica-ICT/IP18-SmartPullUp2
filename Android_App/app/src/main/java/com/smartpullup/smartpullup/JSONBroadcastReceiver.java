be04e325f3acd586decb7e98bcf312e1eab38070
Jorren schepers <s094476@ap.be>
2018-03-17 18:04:55 +0100

    possibly fixed bluetoothIn nullExceptionError

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/JSONBroadcastReceiver.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/JSONBroadcastReceiver.java
@@ -37,7 +37,56 @@ public class JSONBroadcastReceiver extends BroadcastReceiver {
 
 
     public JSONBroadcastReceiver(Handler bluetoothIn) {
-        this.bluetoothIn = bluetoothIn;
+        this.bluetoothIn =new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {              //if string is what we want
+                    String readMessage = (String)msg.obj;                                 // msg.arg1 = bytes from connect thread
+                    recDataString.append(readMessage);                                      //keep appending to string until }
+                    int endOfLineIndex = recDataString.indexOf("}")+1;                    // determine the end-of-line and add the last }
+                    if (endOfLineIndex > 0) {                                           // make sure there data before }
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);    // extract string
+                        //int dataLength = dataInPrint.length();                          //get length of data received
+                        //txtStringLength.setText("String Length = " + String.valueOf(dataLength));
+
+                        if (recDataString.charAt(0) == '{')                             //if it starts with { we know it is what we are looking for
+                        {
+                            jsonData = dataInPrint;             //get sensor value from string
+                            try {
+
+                                jsonObj = new JSONObject(jsonData);
+
+                                String up = jsonObj.getString("up");
+
+                                Log.d("Up data", up);
+
+                                Log.d("Pull up bar", jsonObj.toString());
+
+                            } catch (Throwable t) {
+                                Log.e("Pull up bar", "Could not parse malformed JSON: \"" + jsonData + "\"");
+                            }
+
+                            //sensorView0.setText(jsonData);    //update the textviews with sensor values
+
+                                /*try {
+                                    upView.setText("Up count = " + jsonObj.getString("up"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    downView.setText("Down count = " + jsonObj.getString("down"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }*/
+
+                        }
+                        recDataString.delete(0, recDataString.length());                   //clear all string data
+
+                        dataInPrint = " ";
+
+                    }
+                }
+            }
+        };
         String typeJsonData = null;
         int machine_ID_JsonData = 0;
         int upJsonData = 0;
@@ -64,56 +113,7 @@ public class JSONBroadcastReceiver extends BroadcastReceiver {
             weightJsonData = JSONInputData.getInt("weight");
             Log.i(TAG, "weight= " + weightJsonData);
 
-            bluetoothIn = new Handler() {
-                public void handleMessage(android.os.Message msg) {
-                    if (msg.what == handlerState) {              //if string is what we want
-                        String readMessage = (String)msg.obj;                                 // msg.arg1 = bytes from connect thread
-                        recDataString.append(readMessage);                                      //keep appending to string until }
-                        int endOfLineIndex = recDataString.indexOf("}")+1;                    // determine the end-of-line and add the last }
-                        if (endOfLineIndex > 0) {                                           // make sure there data before }
-                            String dataInPrint = recDataString.substring(0, endOfLineIndex);    // extract string
-                            //int dataLength = dataInPrint.length();                          //get length of data received
-                            //txtStringLength.setText("String Length = " + String.valueOf(dataLength));
 
-                            if (recDataString.charAt(0) == '{')                             //if it starts with { we know it is what we are looking for
-                            {
-                                jsonData = dataInPrint;             //get sensor value from string
-                                try {
-
-                                    jsonObj = new JSONObject(jsonData);
-
-                                    String up = jsonObj.getString("up");
-
-                                    Log.d("Up data", up);
-
-                                    Log.d("Pull up bar", jsonObj.toString());
-
-                                } catch (Throwable t) {
-                                    Log.e("Pull up bar", "Could not parse malformed JSON: \"" + jsonData + "\"");
-                                }
-
-                                //sensorView0.setText(jsonData);    //update the textviews with sensor values
-
-                                /*try {
-                                    upView.setText("Up count = " + jsonObj.getString("up"));
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                try {
-                                    downView.setText("Down count = " + jsonObj.getString("down"));
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }*/
-
-                            }
-                            recDataString.delete(0, recDataString.length());                   //clear all string data
-
-                            dataInPrint = " ";
-
-                        }
-                    }
-                }
-            };
         } catch (JSONException e) {
             e.printStackTrace();
         }
