7d9e0ac7e2a64c0286530bfb997e40888d6a6561
Jorren schepers <s094476@ap.be>
2018-04-16 13:11:52 +0200

    added code to User class to get the currentuser data based on user id

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/User.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/User.java
@@ -1,5 +1,13 @@
 package com.smartpullup.smartpullup;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +16,10 @@ import java.util.List;
  */
 
 public class User {
+    private static final String TAG = "User.class";
+
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
 
     private String id;
     private String firstName;
@@ -18,12 +30,34 @@ public class User {
     public User() {
     }
 
+    public User(String id) {
+        this.id = id;
+        this.exercises = new ArrayList<>();
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("Users/"+id);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User u = dataSnapshot.getValue(User.class);
+                firstName = u.getFirstName();
+                lastName = u.getLastName();
+                email = u.getEmail();
+                exercises = u.getExercises();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "getting userdata from firebase was cancelled");
+            }
+        });
+    }
+
     public User(String id, String firstName, String lastName, String email) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        exercises = new ArrayList<Exercise>();
+        exercises = new ArrayList<>();
     }
 
     public String getId() {
