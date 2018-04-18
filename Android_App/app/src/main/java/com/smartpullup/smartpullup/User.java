5f6a32b79563e3137d959925197f39637b00b657
Jorren schepers <s094476@ap.be>
2018-04-18 20:29:43 +0200

    IMPORTANT FOR APK edited user class for firebase response

--- Android_App/app/src/main/java/com/smartpullup/smartpullup/User.java
+++ Android_App/app/src/main/java/com/smartpullup/smartpullup/User.java
@@ -1,10 +1,25 @@
 package com.smartpullup.smartpullup;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jorren on 1/03/2018.
  */
 
 public class User {
+    private static final String TAG = "User.class";
+
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
 
     private String id;
     private String firstName;
@@ -15,6 +30,10 @@ public class User {
     private String dateBirth;
 
 
+    public User(){
+        //this.exercises = new ArrayList<>();
+    }
+
     public User(String id, String firstName, String lastName, String email, String height, String weight, String dateBirth) {
         this.id = id;
         this.firstName = firstName;
@@ -23,9 +42,32 @@ public class User {
         this.weight = weight;
         this.height = height;
         this.dateBirth = dateBirth;
-
+        //this.exercises = new ArrayList<>();
     }
 
+    public User(String id) {
+        this.id = id;
+        //this.exercises = new ArrayList<>();
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("Users/"+id);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User u = dataSnapshot.getValue(User.class);
+                firstName = u.getFirstName();
+                lastName = u.getLastName();
+                email = u.getEmail();
+                //if(u.getExercises() != null)
+                //    exercises = u.getExercises();
+                Log.i(TAG, "onDataChange: user filled in");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "getting userdata from firebase was cancelled");
+            }
+        });
+    }
     public String getId() {
         return id;
     }
